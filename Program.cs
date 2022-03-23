ef9c40b92be44511c06bfc00472494cba76a5d92
yasmin <yasmin.belci13@gmail.com>
2022-03-23 18:05:08 -0300

    yasmin belci

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome, sobrenome, sobrenomeEmMaiusculas;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiusculas = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
